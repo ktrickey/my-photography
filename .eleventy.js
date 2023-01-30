a4749dc22134e335da255ce4ad008412981b269c
Kevin Trickey <registrations@kevintrickey.me.uk>
2023-01-30 21:16:15 +0000

    passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,16 @@ const
 
 module.exports = eleventyConfig => {
   eleventyConfig.addPassthroughCopy('./src/css');
-  eleventyConfig.addPassthroughCopy('./src/js/isotopes.pkgd.min.js');
+  eleventyConfig.addPassthroughCopy('./src/js/isotope.pkgd.min.js');
+  eleventyConfig.addPassthroughCopy('./src/js/jquery.lazyload.min.js');
+  eleventyConfig.addPassthroughCopy('./src/js/wow.min.js');
+  eleventyConfig.addPassthroughCopy('./src/js/morphext.min.js');
+  eleventyConfig.addPassthroughCopy('./src/js/typed.min.js');
+  eleventyConfig.addPassthroughCopy('./src/js/contact-form.js');
+  eleventyConfig.addPassthroughCopy('./src/js/jquery.ajaxchimp.min.js');
+  eleventyConfig.addPassthroughCopy('./src/js/objectFitPolyfill.min.js');
+  eleventyConfig.addPassthroughCopy('./src/js/splitting.min.js');
+  eleventyConfig.addPassthroughCopy('./src/js/imagesloaded.pkgd.min.js');
   eleventyConfig.addPassthroughCopy('./src/webfonts');
   eleventyConfig.addPassthroughCopy('./src/images');
   eleventyConfig.addWatchTarget('./src/js/');
